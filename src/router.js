312454f55ce96c08fa14866a92c66754cb770050
likang <likang@yaochufa.com>
2019-03-11 14:23:21 +0800

    移除路由懒加载

--- src/router.js
+++ src/router.js
@@ -1,63 +1,64 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import tips from './components/Tips/index'
+import Read from './views/Read.vue'
+import Detail from './views/Detail.vue'
+import User from './views/User.vue'
+import Sociality from './views/Sociality.vue'
+import Followers from './views/Followers.vue'
+import Following from './views/Following.vue'
 
 Vue.use(Router)
 
 const router = new Router({
-    routes: [
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/read',
+      name: 'read',
+      component: Read
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: User
+    },
+    {
+      path: '/sociality',
+      component: Sociality,
+      children: [
         {
-            path: '/',
-            name: 'home',
-            component: Home
+          path: '/',
+          name: 'followers',
+          component: Followers
         },
         {
-            path: '/read',
-            name: 'read',
-            component: () => import('./views/Read.vue')
-        },
-        {
-            path: '/detail',
-            name: 'detail',
-            component: () => import('./views/Detail.vue')
-        },
-        {
-          path: '/user',
-          name: 'user',
-          component: () => import('./views/User.vue')
-        },
-      {
-        path: '/sociality',
-        component: () => import('./views/Sociality.vue'),
-        children: [
-          {
-            // 当 /user/:id/profile 匹配成功，
-            // UserProfile 会被渲染在 User 的 <router-view> 中
-            path: '/',
-            name: 'followers',
-            component: () => import('./views/Followers.vue')
-          },
-          {
-            // 当 /user/:id/posts 匹配成功
-            // UserPosts 会被渲染在 User 的 <router-view> 中
-            path: 'following',
-            name: 'following',
-            component: () => import('./views/Following.vue')
-          }
-        ]
-      }
-    ]
+          path: 'following',
+          name: 'following',
+          component: Following
+        }
+      ]
+    }
+  ]
 })
 
 router.beforeEach((to, from, next) => {
-  if(to.name !== 'read') {
-    if(sessionStorage.getItem("token")) {
+  if (to.name !== 'read') {
+    if (sessionStorage.getItem("token")) {
       next();
-    }else {
+    } else {
       next('/read')
     }
-  }else {
+  } else {
     next();
   }
 })
